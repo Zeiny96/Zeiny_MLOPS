58642c09a1575f18c72b7de9b58cc0d220f219f2
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-04 12:51:51 +0200

    Update test_ui.py

--- test_ui.py
+++ test_ui.py
@@ -20,7 +20,7 @@ def predict(model,img):
     result = "NORMAL" if result >= 0.5 else "COVID19"
     return result
 
-img = "COVID19.jpg"
+img = "COVID19.png"
 for accuracy in ['0.948','0.988']:
     model = load_model('model_0.948.h5')
     result = predict(model,img)
