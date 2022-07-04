937df770b8e32a985ef4be9b92e8c00e630fdff4
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-04 22:04:54 +0200

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -13,7 +13,7 @@ def test_predict(model,img):
     loaded_img = load_image(img)
     loaded_img = np.asarray(loaded_img)
     loaded_img = loaded_img.reshape(1,256,256,1)
-    assert model.predict(loaded_img)[0][0]
+    return model.predict(loaded_img)[0][0]
     
 
 img = "COVID19.png"
