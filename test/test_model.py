58b731630675fb73292ecba650660814ac9503a8
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-04 21:59:20 +0200

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -3,20 +3,20 @@ from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image,ImageOps
 
-def test_load_image(img):
+def load_image(img):
     img = Image.open(img)
     resized_img = img.resize((256,256),resample=Image.Resampling.BILINEAR)
     grayscale_resize_img = ImageOps.grayscale(resized_img)
     return resized_img
 
-def test_predict(accuracy,img):
-    model = load_model(f'../models/model_{accuracy}.h5')
-    loaded_img = test_load_image(img)
+def test_predict(model,img):
+    loaded_img = load_image(img)
     loaded_img = np.asarray(loaded_img)
     loaded_img = loaded_img.reshape(1,256,256,1)
     assert model.predict(loaded_img)[0][0]
     
 
-img = "COVID19.png"
+img = "COVID19.jpg"
 for accuracy in ['0.948','0.988']:
-    test_predict(accuracy,img)
+    model = load_model(f'model_{accuracy}.h5')
+    test_predict(model,img)
