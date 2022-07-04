0e35158a54bff0fba4aaf45156cd86cde93ce0dd
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-05 00:02:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def git_update():
     repo = git.Repo('./Zeiny_MLOPS')
     origin = repo.remotes.origin
     repo.create_head('main',
-                     origin.refs.main).set_tracking_branch(origin.refs.main.api).checkout()
+                     origin.refs.main).set_tracking_branch(origin.refs.api).checkout()
     origin.pull()
     return '', 200
 
