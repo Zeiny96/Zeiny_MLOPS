62c2e814ac3dedabb5e9d270b8537f18104dc2d4
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-04 04:35:55 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+import os
+import io
+from PIL import Image,ImageOps
+from tensorflow.keras.models import load_model
+import numpy as np
+
+def load_image(img):
+    img = Image.open(img)
+    resized_img = img.resize((256,256),resample=Image.BILINEAR)
+    grayscale_resize_img = ImageOps.grayscale(resized_img)
+
+    return grayscale_resize_img
+
+def predict(model,img):
+    loaded_img = load_image(img)
+    loaded_img = np.asarray(loaded_img)
+    loaded_img = loaded_img.reshape(1,256,256,1)
+    result = model.predict(loaded_img)[0][0]
+
+    result = "NORMAL" if result >= 0.5 else "COVID19"
+    return result
+
+img = "COVID19(200).jpg"
+for accuracy in ['0.948','0.988']:
+    model = load_model('model_0.948.h5')
+    result = predict(model,img)
+    print(result)
+
+
