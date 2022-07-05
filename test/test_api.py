816c8170aafd93790a61728fbaee616af4a74a2d
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-05 22:20:47 +0200

    Update test_api.py

--- test/test_api.py
+++ test/test_api.py
@@ -9,11 +9,11 @@ def test_response():
   res_code = str(res).split('[')[1].split(']')[0]
   assert (res_code == '200')
 
-res = requests.get('http://mahmoudelzeiny.pythonanywhere.com/alive')
+res = requests.get('https://zeiny-mlops.herokuapp.com/alive')
 test_alive()
 
 dir="COVID19.png"
 image_file=open(dir,"rb")
 dicttosend={'file':image_file}
-res = requests.post('http://mahmoudelzeiny.pythonanywhere.com/prediction', files=dicttosend)
+res = requests.post('https://zeiny-mlops.herokuapp.com/prediction', files=dicttosend)
 test_response()
