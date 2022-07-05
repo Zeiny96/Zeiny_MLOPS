7dc8da509e0d0d272bbea5fd408dd860d9772a8e
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-05 22:14:35 +0200

    Create test_api.py

---
+++ test/test_api.py
@@ -0,0 +1,19 @@
+import requests
+
+test_alive():
+  res_code = str(res).split('[')[1].split(']')[0]
+  assert (res_code == '200')
+  assert (res.json()['message']=='Server is alive!')
+
+test_response():
+  res_code = str(res).split('[')[1].split(']')[0]
+  assert (res_code == '200')
+
+res = requests.get('http://mahmoudelzeiny.pythonanywhere.com/alive')
+test_alive()
+
+dir="COVID19.png"
+image_file=open(dir,"rb")
+dicttosend={'file':image_file}
+res = requests.post('http://mahmoudelzeiny.pythonanywhere.com/prediction', files=dicttosend)
+test_response()
