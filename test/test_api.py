2fdac305f65fc9d5e8935e3d684250bb0722979f
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-05 23:34:28 +0200

    Update test_api.py

--- test/test_api.py
+++ test/test_api.py
@@ -2,13 +2,45 @@ import requests
 
 def test_response():
   res_code = str(res).split('[')[1].split(']')[0]
-  assert (res_code == '200')
+  assert (res_code == expected_code)
 
+# Alive endpoint test
 res = requests.get('https://zeiny-mlops.herokuapp.com/alive')
+expected_code = '200'
 test_response()
 
+# Prediction endpoint test
 dir="COVID19.png"
 image_file=open(dir,"rb")
 dicttosend={'file':image_file}
 res = requests.post('https://zeiny-mlops.herokuapp.com/prediction', files=dicttosend)
+expected_code = '200'
 test_response()
+
+# Error handling
+## Invalid, empty or corrupted file
+dir="NORMAL.png"
+image_file=open(dir,"rb")
+dicttosend={'file':image_file}
+res = requests.post('https://zeiny-mlops.herokuapp.com/prediction', files=dicttosend)
+expected_code = '400'
+test_response()
+
+## Wrong endpoint
+res = requests.get('https://zeiny-mlops.herokuapp.com/isalive')
+expected_code = '404'
+test_response()
+
+## Wrong method
+### post instead of get
+res = requests.post('https://zeiny-mlops.herokuapp.com/alive')
+expected_code = '405'
+test_response()
+### get instead of post
+dir="COVID19.png"
+image_file=open(dir,"rb")
+dicttosend={'file':image_file}
+res = requests.get('https://zeiny-mlops.herokuapp.com/prediction', files=dicttosend)
+expected_code = '405'
+test_response()
+
