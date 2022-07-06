68ba8a89d13becb5aeae0006d1d5fadf9d851201
Zeiny <62313305+Zeiny96@users.noreply.github.com>
2022-07-06 22:48:32 +0200

    Update test_api.py

--- test/test_api.py
+++ test/test_api.py
@@ -1,12 +1,12 @@
 import requests
 
 def test_response():
-  res_code = res.status_code #str(res).split('[')[1].split(']')[0]
+  res_code = res.status_code
   assert (res_code == expected_code)
 
 # Alive endpoint test
 res = requests.get('https://zeiny-mlops.herokuapp.com/alive')
-expected_code = '200'
+expected_code = 200
 test_response()
 
 # Prediction endpoint test
@@ -14,7 +14,7 @@ dir="COVID19.png"
 image_file=open(dir,"rb")
 dicttosend={'file':image_file}
 res = requests.post('https://zeiny-mlops.herokuapp.com/prediction', files=dicttosend)
-expected_code = '200'
+expected_code = 200
 test_response()
 
 # Error handling
@@ -23,16 +23,16 @@ dir="NORMAL.png"
 image_file=open(dir,"rb")
 dicttosend={'file':image_file}
 res = requests.post('https://zeiny-mlops.herokuapp.com/prediction', files=dicttosend)
-expected_code = '400'
+expected_code = 400
 test_response()
 
 ## Wrong endpoint
 res = requests.get('https://zeiny-mlops.herokuapp.com/isalive')
-expected_code = '404'
+expected_code = 404
 test_response()
 
 ## Wrong method
 res = requests.post('https://zeiny-mlops.herokuapp.com/alive')
-expected_code = '405'
+expected_code = 405
 test_response()
 
